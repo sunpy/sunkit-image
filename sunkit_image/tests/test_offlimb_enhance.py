072780fe4af1a5cd6cac6e19252b5c704aacc3f5
Jack Ireland <jackireland@gmail.com>
2017-12-19 09:40:45 -0500

    tests

--- sunkit_image/tests/test_offlimb_enhance.py
+++ sunkit_image/tests/test_offlimb_enhance.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import
 
 import numpy as np
+from numpy.testing import assert_allclose
 import astropy.units as u
 import sunpy
 import sunpy.map
@@ -28,22 +29,59 @@ def radial_bins():
     """
     Load SunPy's test AIA image.
     """
-    return np.arange(1, 1.5, 0.01) * u.R_sun
+    return np.arange(1, 1.5, 0.1) * u.R_sun
 
 
-def test_radial_bins(r, map_r):
+@pytest.fixture
+def map_r(aia_map):
     """
-
-    Parameters
-    ----------
-    r
-    map_r
-
-    Returns
-    -------
-
+    Load SunPy's test AIA image.
     """
-    if r is None:
-        return np.arange(1, np.max(map_r).to(u.R_sun).value, 0.01) * u.R_sun
+    return find_pixel_radii(aia_map)
+
+
+def test_radial_bins(radial_bins, map_r):
+
+    # Test the default radial bins
+    rb = _radial_bins(None, map_r)
+    assert len(rb) == 50
+    assert rb.unit is u.R_sun
+    assert_allclose((rb[1] - rb[0]).value, 0.01)
+    assert_allclose(rb[0].value, 1.0)
+    assert_allclose(rb[-1].value, np.max(map_r).value)
+
+    # Test another set of
+    rb2 = _radial_bins(radial_bins, map_r)
+    assert len(rb2) == 5
+    assert rb2.unit is u.R_sun
+    assert_allclose((rb2[1] - rb2[0]).value, 0.1)
+    assert_allclose(rb2[0].value, 1.0)
+    assert_allclose(rb2[-1].value, 1.5)
+
+
+def test_bin_edge_summary(radial_bins, binfit='left'):
+
+    # Test
+    bes_left = bin_edge_summary(radial_bins, binfit='left')
+    assert len(bes_left)
+    assert bes_left[0] == 1 * u.
+
+    # Test
+    bes_right = bin_edge_summary(radial_bins, binfit='right')
+    assert len(bes_right)
+
+    # Test
+    bes_center = bin_edge_summary(radial_bins, binfit='center')
+    assert len(bes_center)
+
+    if binfit == 'center':
+        rfit = 0.5 * (r[0:-1] + r[1:])
+    elif binfit == 'left':
+        rfit = r[0:-1]
+    elif binfit == 'right':
+        rfit = r[1:]
     else:
-        return r.to(u.R_sun)
+        raise ValueError(
+            'Keyword "binfit" must have value "center", "left" or "right"')
+    return rfit
+
