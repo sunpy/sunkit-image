0acfc518172db56c869afdca219f34944c1c5657
vatch123 <vatsalyachaubey19980@gmail.com>
2019-05-28 08:59:49 +0530

    Fixed the examples

--- examples/offlimb/radial_gradient_filters.py
+++ examples/offlimb/radial_gradient_filters.py
@@ -1,9 +1,10 @@
 """
-================
-Fourier Normalizing Radial Gradient Filter
-================
+=======================
+Radial Gradient Filters
+=======================
 
-This example applies fourier normalizing gradient filter to a sunpy map.
+This example applies both the normalizing radial gradient filter and fourier 
+normalizing radial gradient filter to a sunpy map.
 
 The example uses `sunkit_image.offlimb` to apply the filter.
 
@@ -27,22 +28,31 @@ from sunkit_image.utils import equally_spaced_bins
 aia_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
 
 ###########################################################################
-# Radial bins are calculated over which the FNRGF filter will be applied.
-# The FNRGF filter is applied, after assuming values for the parameters.
+# Radial bins are calculated over which the filters will be applied.
 
 radial_bin_edges = equally_spaced_bins()
 radial_bin_edges *= u.R_sun
 
+# The NRGF filter is applied after it.
+out1 = offlimb.nrgf(aia_map, radial_bin_edges)
+
+# Assuming values for parameters of FNRGF
 order = 20
 attenuation_coefficients = np.zeros((2, order + 1))
 attenuation_coefficients[0, :] = np.linspace(1, 0, order + 1)
 attenuation_coefficients[1, :] = np.linspace(1, 0, order + 1)
 
-out = offlimb.fnrgf(aia_map, radial_bin_edges, order, attenuation_coefficients)
+# The FNRGF filter is applied after it.
+out2 = offlimb.fnrgf(aia_map, radial_bin_edges, order, attenuation_coefficients)
 
 ###########################################################################
-# The resulting sunpy.map is plotted
+# The resulting sunpy.map are plotted
 
-fig = plt.figure()
-out.plot()
+fig = plt.figure(figsize=(1,2))
+
+ax1 = fig.add_subplot(121, projection=aia_map)
+out1.plot(axes=ax1)
+
+ax2 = fig.add_subplot(122, projection=aia_map)
+out2.plot(axes=ax2)
 plt.show()
