e76573e4aaad6633e2926d608711f97213bb3273
vatch123 <vatsalyachaubey19980@gmail.com>
2019-05-30 19:58:19 +0530

    Added the figure tests for MGN

--- sunkit_image/tests/test_enhance.py
+++ sunkit_image/tests/test_enhance.py
@@ -1,1 +1,24 @@
-# TODO : Write tests for MGN
+import numpy as np
+import matplotlib.pyplot as plt
+import pytest
+
+import sunpy.map
+from sunpy.data.sample import AIA_171_IMAGE
+from sunpy.tests.helpers import figure_test
+
+import sunkit_image.enhance as enhance
+
+
+@pytest.fixture
+def smap():
+    return sunpy.map.Map(AIA_171_IMAGE)
+
+
+@figure_test
+def test_mgn(smap):
+    
+    out = enhance.mgn(smap.data)
+    out = sunpy.map.Map(out, smap.meta)
+
+    fig = plt.figure()
+    out.plot()
