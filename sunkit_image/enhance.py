422ebedf57b0cdd0535a56aca84544def528436b
vatch123 <vatsalyachaubey19980@gmail.com>
2019-06-03 20:02:48 +0530

    fixed pep8

--- sunkit_image/enhance.py
+++ sunkit_image/enhance.py
@@ -24,7 +24,7 @@ def background_supression(image, zmin, qmed=1.0):
 
     Parameters
     ----------
-    image : `numpy.ndarray` 
+    image : `numpy.ndarray`
         Image on which background supression is to be performed.
     zmin : `float`
         The minimum value of intensity which is allowed.
@@ -39,20 +39,20 @@ def background_supression(image, zmin, qmed=1.0):
     """
 
     zmed = np.median(image)
-    
+
     new_image = np.where(image < zmin, qmed * zmed, image)
-    
+
     return new_image
 
 
 def bandpass_filter(image, nsm1=1, nsm2=3):
 
     """
-    Applies a band pass filter to the image. 
+    Applies a band pass filter to the image.
 
     Parameters
     ----------
-    image : `numpy.ndarray` 
+    image : `numpy.ndarray`
         Image to be filtered.
     nsm1 : `int`
         Low pass filter boxcar smoothing constant.
@@ -69,19 +69,19 @@ def bandpass_filter(image, nsm1=1, nsm2=3):
     """
 
     if(nsm1 >= nsm2):
-        raise ValueError ("nsm1 should be less than nsm2")
-    
+        raise ValueError("nsm1 should be less than nsm2")
+
     return ndimage.uniform_filter(image, nsm1) - ndimage.uniform_filter(image, nsm2)
 
 
 def occult2(smap, zmin, num_loop, noise_thresh, qmed=1, nsm1=1, nsm2=3, rmin=30, nmax=1000):
-    
+
     """
     Implements the OCCULT-2 algorithm for loop tracing in solar images.
 
     Parameters
     ----------
-    smap : `sunpy.map` 
+    smap : `sunpy.map`
         Map on which loops are to be detected.
     zmin : `float`
         The minimum value of intensity which is allowed.
@@ -122,14 +122,13 @@ def occult2(smap, zmin, num_loop, noise_thresh, qmed=1, nsm1=1, nsm2=3, rmin=30,
 
     # 2. Bandpass filter
     image = bandpass_filter(image, nsm1, nsm2)
-    
+
     x, y = np.meshgrid(*[np.arange(v.value) for v in smap.dimensions]) * u.pix
     coords = smap.pixel_to_world(x, y).transform_to(frames.Helioprojective)
 
 
-
     # Creating the three starting arrays
-    n_s =100
+    n_s = 100
     delta_s = 1
     s_bi = ((np.arange(n_s) - n_s / 2) * delta_s).reshape((-1, 1))
     s_uni = (delta_s * np.arange(n_s)).reshape((-1, 1))
@@ -156,22 +155,21 @@ def occult2(smap, zmin, num_loop, noise_thresh, qmed=1, nsm1=1, nsm2=3, rmin=30,
         x_0 = coords[i_0, j_0].Tx.value
         y_0 = coords[i_0, j_0].Ty.value
         loop.append([x_0, y_0])
-        
+
         x_k = x_0
         y_k = y_0
 
         for sigma in [-1, 1]:               # To deal with both forward and backward pass
 
             count = 0
-            while count < ngaps :
-                
+            while count < ngaps:
+
                 x_k_l = i_0 + s_bi * np.cos(alp_l.T)
                 y_k_l = j_0 + s_bi * np.sin(alp_l.T)
-                
-                # TODO: Write after understanding the updates. Assume we got # Doing the forward pass
-                sigma = alpha_k
 
-                alpha_k = 12 # arbitrary for the time being
+                # TODO: Write after understanding the updates. Assume we got # Doing the forward pass
+
+                alpha_k = 12  # arbitrary for the time being
 
                 beta_0 = alpha_k + np.pi / 2
 
@@ -182,13 +180,13 @@ def occult2(smap, zmin, num_loop, noise_thresh, qmed=1, nsm1=1, nsm2=3, rmin=30,
                 y_m = y_k + ((y_c - y_0) / rmin) * r_m
 
                 beta_m = beta_0 + sigma * (s_uni / rmin)
-                
+
                 x_k_m = x_m - r_m * np.cos(beta_m)
                 y_k_m = y_m - r_m * np.sin(beta_m)
 
                 # TODO:Calculate the  rm, the radius of curvature
 
-                rm = 5 # For the time being
+                rm = 5  # For the time being
 
                 alpha_k_1 = alpha_k + sigma * (delta_s / rm)
                 alpha_mid = (alpha_k + alpha_k_1) / 2
@@ -196,25 +194,25 @@ def occult2(smap, zmin, num_loop, noise_thresh, qmed=1, nsm1=1, nsm2=3, rmin=30,
                 x_k_1 = x_k + delta_s * np.cos(alpha_mid + (1 + sigma) * np.pi / 2)
                 y_k_1 = y_k + delta_s * np.sin(alpha_mid + (1 + sigma) * np.pi / 2)
 
-                if image(x_k_1,y_k_1) == 0 : # representation of what to do
-                    count += 1 
-                
+                if image(x_k_1, y_k_1) == 0:  # representation of what to do
+                    count += 1
+
                 # Better to convert to pixel values before appending
                 loop.append((x_k_1, y_k_1))
 
                 x_k = x_k_1
                 y_k = y_k_1
-            
+
             if sigma == -1:
                 loop.reverse()
                 x_k = x_0
                 y_k = y_0
-            
-        
+
+
         # Zero out the loop pixels assuming values are stored in pixels format
         for points in loop:
             image[points[0], points[1]] = 0
-         
+
         loops.append(loop)
 
     # Return a map with loops marked. What intensity values to be used for marking loops?
--- sunkit_image/tests/test_enhance.py
+++ sunkit_image/tests/test_enhance.py
@@ -14,44 +14,45 @@ import sunkit_image.enhance as enhance
 def smap():
     return sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
 
+
 @pytest.fixture
 def test_map():
-    map_test = [[1.,1.,1.,1.],
-                [1.,5.,5.,1.],
-                [1.,5.,5.,1.],
-                [1.,1.,1.,1.]]
-    
+    map_test = [[1., 1., 1., 1.],
+                [1., 5., 5., 1.],
+                [1., 5., 5., 1.],
+                [1., 1., 1., 1.]]
+
     return np.array(map_test)
 
 
 def test_background_supression(test_map):
-    
-    expect = [[0.,0.,0.,0.],
-              [0.,5.,5.,0.],
-              [0.,5.,5.,0.],
-              [0.,0.,0.,0.]]
-    
+
+    expect = [[0., 0., 0., 0.],
+              [0., 5., 5., 0.],
+              [0., 5., 5., 0.],
+              [0., 0., 0., 0.]]
+
     result = enhance.background_supression(test_map, 2, 0)
     assert np.allclose(expect, result)
 
-    expect = [[1.,1.,1.,1.],
-              [1.,5.,5.,1.],
-              [1.,5.,5.,1.],
-              [1.,1.,1.,1.]]
-    
+    expect = [[1., 1., 1., 1.],
+              [1., 5., 5., 1.],
+              [1., 5., 5., 1.],
+              [1., 1., 1., 1.]]
+
     result = enhance.background_supression(test_map, 2)
     assert np.allclose(expect, result)
 
 
 @pytest.fixture
 def image():
-    return np.ones((4,4),dtype=float)
+    return np.ones((4, 4), dtype=float)
+
 
 def test_bandpass_filter(image):
 
-    expect = np.zeros((4,4))
+    expect = np.zeros((4, 4))
 
     result = enhance.bandpass_filter(image)
 
     assert np.allclose(expect, result)
-    
