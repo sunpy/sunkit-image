eeaec00ea0c047b52485d3ee58a60d493b16675b
vatch123 <vatsalyachaubey19980@gmail.com>
2019-07-29 21:01:46 +0530

    added examples

---
+++ examples/io_dat_files.py
@@ -0,0 +1,50 @@
+"""
+==================================
+Input/Output of dat files for FLCT
+==================================
+
+This example demostrates the use of functions in `sunkit_image.flct._pyflct`
+to read and write arrays to binary `dat` files.
+"""
+
+import numpy as np
+
+import sunkit_image.flct._pyflct as pyflct
+
+###########################################################################
+# We will create three dummy arrays which will help us demonstrate read
+# and write to a `dat` file.
+
+a = np.zeros((4, 4))
+b = np.ones((4, 4))
+c = np.arange(16).reshape((4, 4))
+
+###########################################################################
+# First, we will demonstrate writing to a `dat` file.
+
+# We can write two arrays to `dat` file using `pyflct.write_two_images`
+pyflct.write_two_images("two.dat", a, b)
+
+# Three arrays can also be written to a `dat` file using `pyflct.write_three_images`
+pyflct.write_three_images("three.dat", a, b, c)
+
+###########################################################################
+# We can get back these arrays by using the read functions in `pyflct`
+# It is to be noted that these read functions can only read special `dat`
+# files, the ones which were written using `pyflct.write_two_images`,
+# `pyflct.read_three_images` and the IDL IO routines as given on the
+# FLCT website.
+
+# Reading two arrays from a `dat` file
+flag, one, two = pyflct.read_two_images("two.dat")
+
+# Reading three arrays
+flag, one, two, three = pyflct.read_three_images("three.dat")
+
+###########################################################################
+# Just to verify what was written and what is read you can also run the below
+# statements.
+
+assert np.allclose(one, a)
+assert np.allclose(two, b)
+assert np.allclose(three, c)
---
+++ examples/running_flct.py
@@ -0,0 +1,35 @@
+"""
+===================================
+Fourier Linear Correlation Tracking
+===================================
+
+This example applies Fourier Linear Correlation Tracking (FLCT)
+to a set of two images taken within a short interval of each other
+using `sunkit_image.flct.flct`.
+"""
+
+import numpy as np
+
+from sunkit_image.flct import flct
+import sunkit_image.data.test as data
+import sunkit_image.flct._pyflct as pyflct
+
+###########################################################################
+# First we need to get the input images which will be read by the
+# `pyflct.read_two_images` from a `dat` file stored in `sunkit_image.data.test`
+
+filepath = data.get_test_filepath("hashgauss.dat")
+flag, image1, image2 = pyflct.read_two_images(filepath)
+
+###########################################################################
+# Now coming to the main function where flct is applied. We will pass the
+# above read arrays to `sunkit_image.flct.flct`
+
+# Since the input arrays were stored in a row major format, `row` was passed
+# as the `order`
+vel_x, vel_y, vel_m = flct(image1, image2, "row", 1, 1, 5, kr=0.5)
+
+###########################################################################
+# The return values are the two dimensional velocity field with `vel_x`
+# showing velocity in the x-direction and `vel_y`, the velocity in the
+# y-direction.
