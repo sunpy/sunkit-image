8a14746b97e22b7ac6e2c6c19450489b0a20f2eb
Vatsalya Chaubey <39871718+vatch123@users.noreply.github.com>
2019-08-17 09:30:00 +0530

    Apply suggestions from code review
    Co-Authored-By: Nabil Freij <nabil.freij@gmail.com>

--- examples/running_flct.py
+++ examples/running_flct.py
@@ -22,7 +22,7 @@ import sunkit_image.flct as flct
 # of the image in some particular directions.
 
 #############################################################################
-# First we plot the velocity field when the image moves in positive ``X`` direction only
+# First we plot the velocity field when the image moves in positive ``X`` direction.
  
 # Creating the input arrays
 image1 = np.zeros((10, 10))
@@ -32,16 +32,16 @@ image2 = np.zeros((10, 10))
 image2[0:3, 1:4] = 1
 
 ###########################################################################
-# Now we come to the main function where FLCT is applied.
-# The values of the parameters were used which gave the best visual result
-# of the velocities. The time difference between the two images, ``deltat`` is assumed
-# to be 1 second. The units of length of the side of a single pixel, ``deltas`` is assumed
-# to be 1. The width of Gaussian used to weigh the subimages, ``sigma`` is taken to be 2.3.
-# Note you should always experiment with the values of ``sigma`` to get the best results.
+# Now we will apply FLCT.
+# The values of the parameters used gave the best visual result
+# of the velocities. The time difference between the two images, ``deltat`` is
+# 1 second. The units of length of the side of a single pixel, ``deltas`` is
+# 1. The width of Gaussian used to weigh the subimages, ``sigma`` is taken to be 2.3.
+# Note you should experiment with the values of ``sigma`` to get the best results.
 vel_x, vel_y, vm = flct.flct(image1, image2, 1, 1, 2.3)
 
 ############################################################################
-# We will plot the 2D flow field what we get when the image have moved in positive ``X`` direction
+# We will plot the 2D flow field what we get when the image have moved in positive ``X`` direction.
 
 # But first we need to create a meshgrid on which the flow field will be plotted
 X = np.arange(0, 10, 1)
@@ -66,7 +66,7 @@ plt.imshow(image2, origin="lower")
 ax3.set_title("Second Image")
 
 #############################################################################
-# Nw, we plot the velocity field when the image moves in positive ``Y`` direction only
+# Nw, we plot the velocity field when the image moves in positive ``Y`` direction.
  
 # Creating the input arrays
 image1 = np.zeros((10, 10))
@@ -85,7 +85,7 @@ image2[1:4, 0:3] = 1
 vel_x, vel_y, vm = flct.flct(image1, image2, 1, 1, 2.3)
 
 ############################################################################
-# We will plot the 2D flow field what we get when the image have moved in positive ``Y`` direction
+# We will plot the 2D flow field what we get when the image have moved in positive ``Y`` direction.
 
 # Plotting the first image
 fig = plt.figure(figsize=(12, 4))
@@ -105,7 +105,7 @@ plt.imshow(image2, origin="lower")
 ax3.set_title("Second Image")
 
 #############################################################################
-# Finally we plot the velocity field when the image moves in both the directions
+# Finally we plot the velocity field when the image moves in both the directions.
  
 # Creating the input arrays
 image1 = np.zeros((10, 10))
@@ -124,7 +124,7 @@ image2[1:4, 1:4] = 1
 vel_x, vel_y, vm = flct.flct(image1, image2, 1, 1, 2.3)
 
 ############################################################################
-# We will plot the 2D flow field what we get when the image have moved in both directions
+# We will plot the 2D flow field what we get when the image have moved in both directions.
 
 # Plotting the first image
 fig = plt.figure(figsize=(12, 4))
@@ -145,8 +145,7 @@ ax3.set_title("Second Image")
 
 ################################################################################
 # Notice the lonely pixel in the bottom left of every flow field which has value less than zero.
-# This discrepancy is due one of the limitations of the `flct` algorithm where it is unable to reliably
+# This discrepancy is due one of the limitations of the FLCT algorithm where it is unable to reliably
 # calculate the velocitites within ``sigma`` pixels of the image edges.
 
-# Show all the plots
 plt.show()
