c58b13de1c75aeddbb0ecc51b27af3aab8e47be2
Vatsalya Chaubey <39871718+vatch123@users.noreply.github.com>
2019-08-18 09:04:23 +0530

    Apply suggestions from code review
    Co-Authored-By: Nabil Freij <nabil.freij@gmail.com>

--- examples/tracing_loops.py
+++ examples/tracing_loops.py
@@ -34,9 +34,10 @@ plt.imshow(image, cmap="hmimag", origin="lower")
 # The length of the smallest loop to be detected ``lmin`` is 25 pixels.
 # The maximum number of structures to be examined ``nstruc`` is 1000.
 # The number of extra points in the loop below noise level to terminate a loop tracing ``ngap`` is 0.
-# The base flux and median flux ratio ``qthresh1`` is 0.0
-# The noise threshold in the image with repect to median flux ``qthresh2`` is 3.0 
-loops = trace.occult2(image, nsm1=3, rmin=30, lmin=25, nstruc=1000, ngap=0, qthresh1=0.0, qthresh2=3.0, file=True)
+# The base flux and median flux ratio ``qthresh1`` is 0.0.
+# The noise threshold in the image with repect to median flux ``qthresh2`` is 3.0 .
+loops = trace.occult2(image, nsm1=3, rmin=30, lmin=25, nstruc=1000, 
+                                  ngap=0, qthresh1=0.0, qthresh2=3.0, file=True)
 
 ###############################################################################
 # `~sunkit_image.trace.occult2` returns a list of all loop where each element
