15879f30f9fdb6ce4238f0fb47d8165cd99f9692
Nabil Freij <nabil.freij@gmail.com>
2019-08-27 11:37:11 +0100

    patch changelog genearation (#39)

--- docs/conf.py
+++ docs/conf.py
@@ -102,15 +102,12 @@ Write the latest changelog into the documentation.
 target_file = os.path.abspath("./whatsnew/latest_changelog.txt")
 try:
     from sunpy.util.towncrier import generate_changelog_for_docs
-
     if is_development:
         generate_changelog_for_docs("../", target_file)
-    else:
-        open(target_file, "a").close()
-except Exception:
-    # If we can't generate it, we need to make sure it exists or else sphinx
-    # will complain.
-    open(target_file, "a").close()
+except Exception as e:
+    print(f"Failed to add changelog to docs with error {e}.")
+# Make sure the file exists or else sphinx will complain.
+open(target_file, 'a').close()
 
 
 def setup(app):
