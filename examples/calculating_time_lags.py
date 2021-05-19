82f5e67521a6dea9639420cff35a950a7eca3ed0
Will Barnes <will.t.barnes@gmail.com>
2021-05-19 16:29:56 -0400

    Apply suggestions from code review
    Co-authored-by: Stuart Mumford <stuart@cadair.com>

--- examples/calculating_time_lags.py
+++ examples/calculating_time_lags.py
@@ -37,12 +37,12 @@ def gaussian_pulse(x, x0, sigma):
 time = np.linspace(0, 1, 500) * u.s
 s_a = gaussian_pulse(time, 0.4 * u.s, 0.02 * u.s)
 s_b = gaussian_pulse(time, 0.6 * u.s, 0.02 * u.s)
+
 plt.plot(time, s_a, label="A")
 plt.plot(time, s_b, label="B")
 plt.xlabel("Time [s]")
 plt.ylabel("Signal")
 plt.legend()
-plt.show()
 
 ###################################################################
 # The problem we are concerned with is how much do we need shift
@@ -107,17 +107,21 @@ s_b = gaussian_pulse(np.tile(time, means_b.shape[1:] + (1,)).T, means_b, 0.02 * 
 
 max_cc_map = max_cross_correlation(s_a, s_b, time)
 tl_map = time_lag(s_a, s_b, time)
+
 fig = plt.figure(figsize=(10, 5))
+
 ax = fig.add_subplot(121)
 im = ax.imshow(tl_map, cmap="RdBu", vmin=-1, vmax=1)
 cax = make_axes_locatable(ax).append_axes("right", size="5%", pad="1%")
 cb = fig.colorbar(im, cax=cax)
 cb.set_label(r"$\tau_{AB}$ [s]")
+
 ax = fig.add_subplot(122)
 im = ax.imshow(max_cc_map, vmin=0, vmax=1, cmap="magma")
 cax = make_axes_locatable(ax).append_axes("right", size="5%", pad="1%")
 cb = fig.colorbar(im, cax=cax)
 cb.set_label(r"Max cross-correlation")
+
 plt.tight_layout()
 plt.show()
 
