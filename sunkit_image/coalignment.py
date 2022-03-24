752d5968605584a0596fa615f4f815a87570749e
Will Barnes <will.t.barnes@gmail.com>
2022-03-23 20:35:29 -0400

    Move `sunpy.physics.solar_rotation` to `sunkit-image.coalignment` (#81)
    * Deprecate transforms module and move contents to sunpy.physics
    
    * Adjusted the testing tolerances and values for test_solar_rotation.py
    
    * updated mapcube solar rotation tests to take account of new calculation of center of the FOV
    
    * Change coordinate frames to have an observer attribute
    
    This means that the HGS location of the observer for HCC and HPC frames
    is now a HeliographicStonyhurst coordinate object. This avoids any
    ambiguity between L0 and Phi for the notation of heliographic longitude.
    
    This then also fixes all the tests for the changes to Map and coordinates.
    
    * Make submap work correctly for rotated maps.
    
    submap now:
    
    * Take input as `bottom_left` and `top_right`
    * Only allows physical coordinate input in form or one or two `SkyCoord` objects
    * Issues a warning when using pixel coordinates due to the behaviour change.
    
    Also fix tests for other changes to map
    
    * Convert .lon and .lat to axis1 and axis2
    
    Also replace all references in the code base to use tuple indexing rather than
    the names.
    
    * my changes extracted from messed up branch
    
    * more code ref improvements
    
    * Address comments from @solardrew
    
    * JSOC BROKE THIS
    
    * Backward compatibility additions
    
    * add @dep for Mapcube and in a few other places
    
    * comments addressed
    
    * more comments addressed
    
    * change cube to sequence
    
    * Removed everything dep code and warning.
    No more MapCube as well.
    
    * simple stiff solar rotation of a mapsequence now takes account of the new solar_rotate_coordinate
    
    * fixed tests due to change in IAU
    
    * fixing tests
    
    * oops, forgot to fix these too
    
    * revert numbers
    
    * updated tests due to changes due to merge
    
    * Fix tests that were affected by the change in AIA observer coordinate
    
    * fix conflict
    
    * spaces
    
    * fixing solar rotation tests
    
    * update test
    
    * added 32bit skip for tests
    
    * Fixed bug with trying to shift a layer relative to itself
    
    * Fix submap tests
    
    * Apply isort
    
    * autopep8 and expand flake8 check
    
    * addressed comments
    
    * Fixed existing differential-rotation tests
    
    * Misc docstring fixes
    
    * private versions of calculate_clipping and clip_edges for mapsequence_solar_derotate
    
    * put everything in coalignment
    
    * changelog
    
    * fix up docs; rename mapsequence_solar_derotate to mapsequence_coalign_by_rotation
    
    * make figure devdeps happy
    
    * suggestions from code review
    
    * fix test helper import
    
    * fix docstring list
    
    Co-authored-by: Igor Babuschkin <igor@babuschk.in>
    Co-authored-by: Albert Y. Shih <ayshih@gmail.com>
    Co-authored-by: Stuart Mumford <stuart@cadair.com>
    Co-authored-by: Jack Ireland <jackireland@gmail.com>
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>
    Co-authored-by: mskirk <michael.s.kirk@gmail.com>
    Co-authored-by: wafels <jackreland@gmail.com>
    Co-authored-by: David Stansby <dstansby@gmail.com>

--- sunkit_image/coalignment.py
+++ sunkit_image/coalignment.py
@@ -1,21 +1,9 @@
 """
 This module provides routines for the co-alignment of images and
-`~sunpy.map.mapsequence.MapSequence`.
-
-Currently this module provides image co-alignment by template matching.
-Which is partially inspired by the SSWIDL routine
-`tr_get_disp.pro <http://www.heliodocs.com/php/xdoc_print.php?file=$SSW/trace/idl/util/tr_get_disp.pro>`__.
-
-In this implementation, the template matching is handled via the scikit-image
-routine `skimage.feature.match_template`.
-
-References
-----------
-* http://scribblethink.org/Work/nvisionInterface/nip.html
-* J.P. Lewis, Fast Template Matching, Vision Interface 95, Canadian Image
-   Processing and Pattern Recognition Society, Quebec City, Canada, May 15-19,
-   1995, p. 120-123 http://www.scribblethink.org/Work/nvisionInterface/vi95_lewis.pdf.
+`~sunpy.map.mapsequence.MapSequence` objects through both template
+matching and corrections due to solar rotation.
 """
+import warnings
 from copy import deepcopy
 
 import numpy as np
@@ -25,7 +13,10 @@ from skimage.feature import match_template
 import astropy.units as u
 import sunpy.map
 from sunpy.map.mapbase import GenericMap
-from sunpy.util.exceptions import warn_user
+from sunpy.physics.differential_rotation import solar_rotate_coordinate
+from sunpy.util.exceptions import SunpyUserWarning
+
+__author__ = "J. Ireland"
 
 __all__ = [
     "calculate_shift",
@@ -39,6 +30,8 @@ __all__ = [
     "apply_shifts",
     "mapsequence_coalign_by_match_template",
     "calculate_match_template_shift",
+    "calculate_solar_rotate_shift",
+    "mapsequence_coalign_by_rotation",
 ]
 
 
@@ -308,21 +301,23 @@ def check_for_nonfinite_entries(layer_image, template_image):
         A two-dimensional `numpy.ndarray`.
     """
     if not np.all(np.isfinite(layer_image)):
-        warn_user(
+        warnings.warn(
             "The layer image has nonfinite entries. "
             "This could cause errors when calculating shift between two "
             "images. Please make sure there are no infinity or "
             "Not a Number values. For instance, replacing them with a "
-            "local mean."
+            "local mean.",
+            SunpyUserWarning,
         )
 
     if not np.all(np.isfinite(template_image)):
-        warn_user(
+        warnings.warn(
             "The template image has nonfinite entries. "
             "This could cause errors when calculating shift between two "
             "images. Please make sure there are no infinity or "
             "Not a Number values. For instance, replacing them with a "
-            "local mean."
+            "local mean.",
+            SunpyUserWarning,
         )
 
 
@@ -473,18 +468,24 @@ def calculate_match_template_shift(mc, template=None, layer_index=0, func=_defau
     return {"x": xshift_arcseconds, "y": yshift_arcseconds}
 
 
-# Coalignment by matching a template
 def mapsequence_coalign_by_match_template(
     mc, template=None, layer_index=0, func=_default_fmap_function, clip=True, shift=None, **kwargs
 ):
     """
     Co-register the layers in a `~sunpy.map.MapSequence` according to a
-    template taken from that `~sunpy.map.MapSequence`. This method REQUIRES
-    that scikit-image be installed. When using this functionality, it is a good
-    idea to check that the shifts that were applied to were reasonable and
+    template taken from that `~sunpy.map.MapSequence`.
+
+    When using this functionality, it is a good
+    idea to check that the shifts that were applied were reasonable and
     expected. One way of checking this is to animate the original
     `~sunpy.map.MapSequence`, animate the coaligned `~sunpy.map.MapSequence`,
     and compare the differences you see to the calculated shifts.
+
+    Currently this module provides image co-alignment by template matching.
+    and is partially inspired by the SSWIDL routine
+    `tr_get_disp.pro <http://www.heliodocs.com/php/xdoc_print.php?file=$SSW/trace/idl/util/tr_get_disp.pro>`__.
+    In this implementation, the template matching is handled via
+    `skimage.feature.match_template`.
 
     Parameters
     ----------
@@ -528,7 +529,7 @@ def mapsequence_coalign_by_match_template(
 
     Notes
     -----
-    The remaining keyword arguments are sent to `sunpy.image.coalignment.apply_shifts`.
+    The remaining keyword arguments are sent to `~sunkit_image.coalignment.apply_shifts`.
 
     Returns
     -------
@@ -544,6 +545,13 @@ def mapsequence_coalign_by_match_template(
     >>> coaligned_mc = mc_coalign(mc, template=sunpy_map)   # doctest: +SKIP
     >>> coaligned_mc = mc_coalign(mc, template=two_dimensional_ndarray)   # doctest: +SKIP
     >>> coaligned_mc = mc_coalign(mc, func=np.log)   # doctest: +SKIP
+
+    References
+    ----------
+    * http://scribblethink.org/Work/nvisionInterface/nip.html
+    * J.P. Lewis, Fast Template Matching, Vision Interface 95, Canadian Image
+      Processing and Pattern Recognition Society, Quebec City, Canada, May 15-19,
+      1995, p. 120-123 http://www.scribblethink.org/Work/nvisionInterface/vi95_lewis.pdf.
     """
     # Number of maps
     nt = len(mc.maps)
@@ -567,3 +575,122 @@ def mapsequence_coalign_by_match_template(
 
     # Apply the shifts and return the coaligned mapsequence
     return apply_shifts(mc, -yshift_keep, -xshift_keep, clip=clip, **kwargs)
+
+
+def calculate_solar_rotate_shift(mc, layer_index=0, **kwargs):
+    """
+    Calculate the shift that must be applied to each map contained in a mapsequence
+    in order to compensate for solar rotation.
+
+    The center of the map is used to calculate the position of each mapsequence
+    layer. Shifts are calculated relative to a specified layer in the mapsequence.
+    When using this functionality, it is a good idea to check that the shifts
+    that were applied to were reasonable and expected. One way of checking this
+    is to animate the original mapsequence, animate the derotated mapsequence, and
+    compare the differences you see to the calculated shifts. An example use is
+    as follows. If you select data from the SDO cutout service, it is common to
+    not use the solar tracking implemented by this service. This is because (at
+    time of writing) the solar tracking implemented by that service moves the
+    image by single pixels at a time. This is not optimal for many use cases,
+    as it introduces artificial jumps in the data. So with solar tracking not
+    chosen, the selected area is like a window through which you can see the
+    Sun rotating underneath.
+
+    Parameters
+    ----------
+    mc : `sunpy.map.MapSequence`
+        The input mapsequence.
+    layer_index : int
+        The index layer.  Shifts are calculated relative to the time of
+        this layer.
+    ``**kwargs``
+        These keywords are passed to the function
+        `sunpy.physics.differential_rotation.solar_rotate_coordinate`.
+    Returns
+    -------
+    `~astropy.units.Quantity`, ~astropy.units.Quantity`
+        The shifts relative to the index layer that can be applied
+        to the input mapsequence in order to compensate for solar rotation.
+        The shifts are given in arcseconds as understood in helioprojective
+        coordinates systems.
+    """
+    nt = len(mc.maps)
+    xshift_arcseconds = np.zeros(nt) * u.arcsec
+    yshift_arcseconds = np.zeros_like(xshift_arcseconds)
+    rotate_to_this_layer = mc.maps[layer_index]
+
+    for i, m in enumerate(mc):
+        # The shift of the reference layer is always zero by definition.
+        if i == layer_index:
+            continue
+
+        # Calculate the rotation of the center of the map 'm' at its
+        # observation time to the observation time of the reference layer
+        # indicated by "layer_index".
+        new_coordinate = solar_rotate_coordinate(
+            m.center, observer=rotate_to_this_layer.observer_coordinate, **kwargs
+        )
+
+        xshift_arcseconds[i] = new_coordinate.Tx - rotate_to_this_layer.center.Tx
+        yshift_arcseconds[i] = new_coordinate.Ty - rotate_to_this_layer.center.Ty
+
+    return {"x": xshift_arcseconds, "y": yshift_arcseconds}
+
+
+def mapsequence_coalign_by_rotation(mc, layer_index=0, clip=True, shift=None, **kwargs):
+    """
+    Move the layers in a mapsequence according to the input shifts.
+
+    If an input shift is not given, the shifts due to
+    solar rotation relative to an index layer is calculated and
+    applied. When using this functionality, it is a good idea to check
+    that the shifts that were applied to were reasonable and expected.
+    One way of checking this is to animate the original mapsequence, animate
+    the derotated mapsequence, and compare the differences you see to the
+    calculated shifts.
+
+    Parameters
+    ----------
+    mc : `sunpy.map.MapSequence`
+        A mapsequence of shape (ny, nx, nt), where nt is the number of layers in
+        the mapsequence.
+    layer_index : int
+        Solar derotation shifts of all maps in the mapsequence are assumed
+        to be relative to the layer in the mapsequence indexed by ``layer_index``.
+    clip : bool
+        If True, then clip off x, y edges in the datasequence that are potentially
+        affected by edges effects.
+    ``**kwargs``
+        These keywords are passed to
+        `~sunkit_image.coalignment.calculate_solar_rotate_shift`.
+
+    Returns
+    -------
+    `sunpy.map.MapSequence`
+        The results of the shifts applied to the input mapsequence.
+
+    Examples
+    --------
+    >>> import sunpy.data.sample  # doctest: +REMOTE_DATA
+    >>> from sunpy.physics.solar_rotation import mapsequence_solar_derotate
+    >>> map1 = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)  # doctest: +REMOTE_DATA
+    >>> map2 = sunpy.map.Map(sunpy.data.sample.EIT_195_IMAGE)  # doctest: +REMOTE_DATA
+    >>> mc = sunpy.map.Map([map1, map2], sequence=True)  # doctest: +REMOTE_DATA
+    >>> derotated_mc = mapsequence_solar_derotate(mc)  # doctest: +SKIP
+    >>> derotated_mc = mapsequence_solar_derotate(mc, layer_index=-1)  # doctest: +SKIP
+    >>> derotated_mc = mapsequence_solar_derotate(mc, clip=False)  # doctest: +SKIP
+    """
+    nt = len(mc.maps)
+    xshift_keep = np.zeros(nt) * u.pix
+    yshift_keep = np.zeros_like(xshift_keep)
+
+    if shift is None:
+        shift = calculate_solar_rotate_shift(mc, layer_index=layer_index, **kwargs)
+    xshift_arcseconds = shift["x"]
+    yshift_arcseconds = shift["y"]
+
+    for i, m in enumerate(mc):
+        xshift_keep[i] = xshift_arcseconds[i] / m.scale[0]
+        yshift_keep[i] = yshift_arcseconds[i] / m.scale[1]
+
+    return apply_shifts(mc, yshift_keep, xshift_keep, clip=clip)
