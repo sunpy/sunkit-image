d55a2fe07aa962ca7a7427a72bb4fb65371bd04b
Nabil Freij <nabil.freij@gmail.com>
2022-09-06 13:08:41 +0100

    ci

--- sunkit_image/tests/test_enhance.py
+++ sunkit_image/tests/test_enhance.py
@@ -32,12 +32,10 @@ def test_multiscale_gaussian(map_test):
     # Assuming the algorithm works fine then the below two should be equal.
     expect1 = enhance.mgn(map_test, [1])
     expect2 = enhance.mgn(map_test, [1, 1])
-
     assert np.allclose(expect1, expect2)
 
     result1 = np.zeros((4, 4), dtype=float)
     expect3 = enhance.mgn(map_test, [1])
-
     assert np.allclose(result1, expect3)
 
     # This is a dummy test. These values were not verified by hand rather they were
@@ -50,9 +48,7 @@ def test_multiscale_gaussian(map_test):
             [0.0305363, 0.0305363, 0.0305363, 0.0305363],
         ]
     )
-
     expect4 = enhance.mgn(map_test)
-
     assert np.allclose(result2, expect4)
 
 
