30d67d017dec89a6811e0bae8fedf35ae4dad33e
LeahDZuckerman <LeahDZuckerman@gmail.com>
2023-08-03 16:03:33 -0600

    update test_trim_intergranules to reflect that padding will introduce many zeros on outside edge

--- sunkit_image/tests/test_granule.py
+++ sunkit_image/tests/test_granule.py
@@ -62,9 +62,10 @@ def test_trim_intergranules(granule_map):
     # Check that the correct dimensions are returned.
     assert thresholded.shape == _trim_intergranules(thresholded).shape
     # Check that erroneous zero values are caught and re-assigned
-    # e.g. the returned array has fewer (or same number) 0-valued pixels as input
+    # e.g. inside of pad region, returned array has fewer 0-valued pixels then input
     middles_removed = _trim_intergranules(thresholded)
-    assert not np.count_nonzero(middles_removed) < np.count_nonzero(thresholded)
+    pad = int(np.shape(thresholded)[0]/200)
+    assert not np.count_nonzero(middles_removed[pad:-pad, pad:-pad]) < np.count_nonzero(thresholded[pad:-pad, pad:-pad])
     # Check that when mark=True, erroneous 0 values are set to 3
     middles_marked = _trim_intergranules(thresholded, mark=True)
     marked_as_3 = np.count_nonzero(middles_marked[middles_marked == 3])
