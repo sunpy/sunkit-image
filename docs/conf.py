3b145b5d1511008890837d3a794d6c3092956c73
CyclingNinja <bennett.sm89@gmail.com>
2024-06-18 21:52:43 +0100

    Addressing PR omments

--- docs/conf.py
+++ docs/conf.py
@@ -1,25 +1,32 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+"""
+Configuration file for the Sphinx documentation builder.
+"""
 
 import datetime
 import os
 import pathlib
 
-# -- Project information -----------------------------------------------------
+from packaging.version import Version
+from sunpy_sphinx_theme.conf import *  # NOQA: F403
 
-# The full version, including alpha/beta/rc tags
 from sunkit_image import __version__
 
-release = __version__
+# -- Read the Docs Specific Configuration --------------------------------------
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+if on_rtd:
+    os.environ["SUNPY_CONFIGDIR"] = "/home/docs/"
+    os.environ["HOME"] = "/home/docs/"
+    os.environ["LANG"] = "C"
+    os.environ["LC_ALL"] = "C"
+    os.environ["HIDE_PARFIVE_PROGESS"] = "True"
 
 project = "sunkit_image"
 author = "The SunPy Community"
-copyright = f"{datetime.datetime.now().year}, {author}"  # noqa: A001
+copyright = f"{datetime.datetime.now(datetime.UTC).year}, {author}"  # NOQA: A001
 
-# -- General configuration ---------------------------------------------------
+release = __version__
+sunkit_image_version = Version(__version__)
+is_release = not (sunkit_image_version.is_prerelease or sunkit_image_version.is_devrelease)
 
 # For the linkcheck
 linkcheck_ignore = [
@@ -44,9 +51,6 @@ suppress_warnings = [
     "app.add_directive",
 ]
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
-# ones.
 extensions = [
     "sphinx_design",
     "sphinx_gallery.gen_gallery",
@@ -57,34 +61,47 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
     "sphinx.ext.doctest",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
     "sphinx.ext.inheritance_diagram",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
     "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
 ]
-
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 html_extra_path = ["robots.txt"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 source_suffix = ".rst"
 master_doc = "index"
-
-# Treat everything in single ` as a Python reference.
-default_role = 'py:obj'
-
-
-# -- Options for intersphinx extension ---------------------------------------
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"python": ("https://docs.python.org/", None)}
+default_role = "obj"
+napoleon_use_rtype = False
+napoleon_google_docstring = False
+intersphinx_mapping = {
+    "python": (
+        "https://docs.python.org/3/",
+        (None, "http://www.astropy.org/astropy-data/intersphinx/python3.inv"),
+    ),
+    "numpy": (
+        "https://numpy.org/doc/stable/",
+        (None, "http://www.astropy.org/astropy-data/intersphinx/numpy.inv"),
+    ),
+    "scipy": (
+        "https://docs.scipy.org/doc/scipy/reference/",
+        (None, "http://www.astropy.org/astropy-data/intersphinx/scipy.inv"),
+    ),
+    "matplotlib": ("https://matplotlib.org/stable", None),
+    "sunpy": (
+        "https://sunpy.org/",
+        (None, "https://docs.sunpy.org/en/stable/"),
+    ),
+    "astropy": ("https://docs.astropy.org/en/stable/", None),
+    "sqlalchemy": ("https://docs.sqlalchemy.org/en/latest/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "skimage": ("https://scikit-image.org/docs/stable/", None),
+    "drms": ("https://docs.sunpy.org/projects/drms/en/stable/", None),
+    "parfive": ("https://parfive.readthedocs.io/en/stable/", None),
+    "reproject": ("https://reproject.readthedocs.io/en/stable/", None),
+    "aiapy": ("https://aiapy.readthedocs.io/en/stable/", None),
+}
 
 # -- Options for HTML output ---------------------------------------------------
 graphviz_output_format = "svg"
