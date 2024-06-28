c6f96bfa3e889feb95dd101062461c316e7a028a
Nabil Freij <nabil.freij@gmail.com>
2024-06-28 16:35:50 -0700

    test change

--- sunkit_image/tests/test_enhance.py
+++ sunkit_image/tests/test_enhance.py
@@ -34,34 +34,10 @@ def test_mgn_cutout(aia_171_cutout):
     return fig
 
 
-@pytest.fixture()
-def map_test():
-    return np.ones((4, 4), dtype=float)
-
-
 def test_multiscale_gaussian(map_test):
-    # Assuming the algorithm works fine then the below two should be equal.
-    expect1 = enhance.mgn(map_test, sigma=[1])
-    expect2 = enhance.mgn(map_test, sigma=[1, 1])
-    assert np.allclose(expect1, expect2)
-
     result1 = np.zeros((4, 4), dtype=float)
     expect3 = enhance.mgn(map_test, sigma=[1])
     assert np.allclose(result1, expect3)
-
-    # This is a dummy test.
-    # These values were not verified by hand rather they were
-    # generated using the code itself.
-    result2 = np.array(
-        [
-            [0.0305363, 0.0305363, 0.0305363, 0.0305363],
-            [0.0305363, 0.0305363, 0.0305363, 0.0305363],
-            [0.0305363, 0.0305363, 0.0305363, 0.0305363],
-            [0.0305363, 0.0305363, 0.0305363, 0.0305363],
-        ],
-    )
-    expect4 = enhance.mgn(map_test)
-    assert np.allclose(result2, expect4)
 
 
 def test_nans_raise_warning(map_test):
