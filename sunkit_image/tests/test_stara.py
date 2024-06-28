7643a41ad8d79c65f135cd835e1a06e61d48cc4a
Deus1704 <jayraj.jayraj@iitgn.ac.in>
2024-06-28 20:15:35 +0530

    added a limb filter and threshold test

--- sunkit_image/tests/test_stara.py
+++ sunkit_image/tests/test_stara.py
@@ -14,6 +14,21 @@ def test_stara(hmi_map):
     assert total_true_value_count == 5033
 
 
+def test_stara_threshold_adjustment(hmi_map):
+    hmi_upscaled = hmi_map.resample((512, 512) * u.pixel)
+    # Apply STARA with a lower threshold, expecting to detect more features
+    lower_threshold_result = stara(hmi_upscaled, threshold=2000)
+    higher_threshold_result = stara(hmi_upscaled, threshold=8000)
+    # A lower limb filter, would detect more features
+    lower_limb_filtered_result = stara(hmi_upscaled, limb_filter=5 * u.percent)
+    higher_limb_filtered_result = stara(hmi_upscaled, limb_filter=30 * u.percent)
+    # Assert that the lower threshold results in more features detected
+    assert lower_threshold_result.sum() > higher_threshold_result.sum(), "Lower threshold should detect more features"
+    assert (
+        lower_limb_filtered_result.sum() > higher_limb_filtered_result.sum()
+    ), "Lower Limb filter should detect more features"
+
+
 @figure_test
 def test_stara_plot(hmi_map):
     import matplotlib.pyplot as plt
