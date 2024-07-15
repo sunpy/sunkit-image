7dba720a08268dbbb2e1de218a9a1e2b2d843aba
Deus1704 <jayraj.jayraj@iitgn.ac.in>
2024-07-15 15:28:16 +0530

    fix the issue of method not being registered

--- sunkit_image/coalignment/__init__.py
+++ sunkit_image/coalignment/__init__.py
@@ -1,3 +1,6 @@
 from sunkit_image.coalignment.interface import coalignment
 
+# To extend the package with new coalignment methods, please follow the import style shown below.
+from sunkit_image.coalignment.match_template import match_template_coalign  # noqa: F401
+
 __all__ = ["coalignment"]
