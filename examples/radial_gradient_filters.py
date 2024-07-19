83057d16fe2c2e6005a4d78e9768a5f87deb6a57
Chris R. Gilly <gilly@swri.org>
2024-07-19 14:45:46 -0600

    Implementing the RHE Filter (#205)
    * instantiate the RHEF
    
    * move helper functions to util file
    
    * make the most basic test
    
    * combine the two layers of abstraction
    
    * update the test run
    
    * update first test to sunpy standards
    
    * remove private functions in __all__
    
    * make private functions public for now
    
    * fix TypeError
    
    * add progress bar
    
    * update rhef tests
    
    * update hash file for rhef
    
    * remove test_rhef and fix test_multifig_rhef
    
    * fix the multifig test
    
    * update the figure hash
    
    * tweak figure parameters
    
    * fix broken test
    
    * fix test again
    
    * update figure hash
    
    * instantiate the RHEF example
    
    * improve example formatting
    
    * improve the rhef example
    
    * update rhef example
    
    * tox codestyle
    
    * fix tox again
    
    * remove errant test call
    
    * Apply suggestions from code review
    
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>
    
    * remove clipping and replace with norm
    
    * update the example and clean up some comments
    
    * fix broken tox codestyle
    
    * capitalize the method name
    
    * add optional vignette util
    
    * fix tox codestyle
    
    * add vignette to docstring
    
    * Clarify the NRGF example and fix normalization
    
    * add tqdm to the rgfs
    
    * update docstring
    
    * enable disabling of progressbar
    
    * apply suggestions
    
    * address comments about quantity inputs
    
    * Apply suggestions from code review
    
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>
    
    * fix tox errors
    
    * fix failing test
    
    * update hashes
    
    * fix vignette and docs
    
    * fix figure 1
    
    * fix hashes
    
    * Apply suggestions from code review
    
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>
    
    * Apply suggestions from code review
    
    stop requiring a change in normalization to plot correctly
    
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>
    
    * fix map normalization
    
    * fix tests
    
    * fix docstring for test
    
    * Apply suggestions from code review
    
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>
    
    * add stara_plot to hash
    
    * fix docstring test failure
    
    * update changelog
    
    * pull helper functions into private methods
    
    * crop images
    
    * write and test new functionalized version
    
    * changelog and cosmetic changes
    
    * fix logging error
    
    * respond to code review comments
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update figure_hashes_mpl_390_ft_261_sunpy_513_astropy_610.json
    
    * refs
    
    * fix note
    
    ---------
    
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- examples/radial_gradient_filters.py
+++ examples/radial_gradient_filters.py
@@ -1,10 +1,11 @@
 """
-=======================
-Radial Gradient Filters
-=======================
+===================================
+Normalizing Radial Gradient Filters
+===================================
 
-This example applies both the normalizing radial gradient (`sunkit_image.radial.nrgf`) filter and Fourier
-normalizing radial gradient filter (`sunkit_image.radial.fnrgf`) to a sunpy map.
+This example showcases the filters found in the radial module.
+
+These are the Normalizing Radial Gradient Filter (NRGF) (`sunkit_image.radial.nrgf`), Fourier Normalizing Radial Gradient Filter (FNRGF) (`sunkit_image.radial.fnrgf`) and the Radial Histogram Equalizing Filter (RHEF) (`sunkit_image.radial.rhef`).
 """
 
 import matplotlib.pyplot as plt
@@ -31,20 +32,19 @@ aia_map.plot(clip_interval=(1, 99.99) * u.percent)
 # Both the NRGF and FNRGF work on radial segments above their application radius.
 # Here we create those segments radial segments. Each segment created will be of
 # equal dimensions radially. The distance between 1 solar radii and 2 solar radii
-# is divided into 100 equal parts by the following two lines.
+# is divided into equal parts by the following two lines.
 
-radial_bin_edges = equally_spaced_bins()
+radial_bin_edges = equally_spaced_bins(1, 2, aia_map.data.shape[0] // 4)
 radial_bin_edges *= u.R_sun
 
 # The NRGF filter is applied after it.
-out1 = radial.nrgf(aia_map, radial_bin_edges)
+out1 = radial.nrgf(aia_map, radial_bin_edges, application_radius=1 * u.R_sun)
 
 # The NRGF filtered map is plotted.
-# The image seems a little washed out so you may need to change some plotting settings
-# for a clearer output.
 fig = plt.figure()
 ax = plt.subplot(projection=out1)
 out1.plot()
+ax.set_title("NRGF")
 
 ###########################################################################
 # We will need to work out  a few parameters for the FNRGF.
@@ -56,11 +56,23 @@ order = 20
 attenuation_coefficients = radial.set_attenuation_coefficients(order)
 
 # The FNRGF filter is applied after it.
-out2 = radial.fnrgf(aia_map, radial_bin_edges, order, attenuation_coefficients)
+out2 = radial.fnrgf(aia_map, radial_bin_edges, order, attenuation_coefficients, application_radius=1 * u.R_sun)
 
 # The FNRGF filtered map is plotted.
 fig = plt.figure()
 ax = plt.subplot(projection=out2)
 out2.plot()
+ax.set_title("FNRGF")
+
+###########################################################################
+# We can compare to the RHEF as well.
+
+rhef_map = radial.rhef(aia_map, radial_bin_edges, application_radius=1 * u.R_sun)
+
+# The RHE filtered map is plotted.
+fig = plt.figure()
+ax = plt.subplot(projection=rhef_map)
+rhef_map.plot()
+ax.set_title("RHEF")
 
 plt.show()
---
+++ examples/radial_histogram_equalization.py
@@ -0,0 +1,113 @@
+"""
+=============================
+Radial Histogram Equalization
+=============================
+
+This example applies the Radial Histogram Equalizing Filter (`sunkit_image.radial.rhef`) to a sunpy map.
+"""
+
+import matplotlib.pyplot as plt
+
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+
+import sunpy.data.sample
+import sunpy.map
+
+import sunkit_image.enhance as enhance
+import sunkit_image.radial as radial
+from sunkit_image.utils import equally_spaced_bins
+
+#######################################################################################
+# Let us use the sunpy sample data AIA image to showcase the RHE filter.
+aia_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE, autoalign=True)
+
+# Create radial segments (RHEF should use a dense grid)
+radial_bin_edges = equally_spaced_bins(0, 2, aia_map.data.shape[0] // 2)
+radial_bin_edges *= u.R_sun
+
+rhef_map = radial.rhef(aia_map, radial_bin_edges)
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 5), sharex="all", sharey="all", subplot_kw={"projection": aia_map})
+
+aia_map.plot(axes=axes[0], clip_interval=(1, 99.99) * u.percent)
+axes[0].set_title("Original AIA Map")
+
+rhef_map.plot(axes=axes[1])
+axes[1].set_title(r"RHE Filtered Map, $\Upsilon$=0.35")
+
+fig.tight_layout()
+
+#######################################################################################
+# The RHEF has one free parameter that works in post processing to modulate the output.
+# Here are some of the choices one could make.
+# `See the thesis (Gilly 2022) Eq 4.15 for details about upsilon. <https://www.proquest.com/docview/2759080511>`__
+
+# Define the list of upsilon pairs where the first number affects dark components and the second number affects bright ones
+upsilon_list = [
+    0.35,
+    None,
+    (0.1, 0.1),
+    (0.5, 0.5),
+    (0.8, 0.8),
+]
+
+# Crop the figures to see better detail
+top_right = SkyCoord(1200 * u.arcsec, 0 * u.arcsec, frame=aia_map.coordinate_frame)
+bottom_left = SkyCoord(0 * u.arcsec, -1200 * u.arcsec, frame=aia_map.coordinate_frame)
+aia_map_cropped = aia_map.submap(bottom_left, top_right=top_right)
+fig, axes = plt.subplots(2, 3, figsize=(15, 10), sharex="all", sharey="all", subplot_kw={"projection": aia_map_cropped})
+axes = axes.flatten()
+
+aia_map_cropped.plot(axes=axes[0], clip_interval=(1, 99.99) * u.percent)
+axes[0].set_title("Original AIA Map")
+
+# Loop through the upsilon_list and plot each filtered map
+for i, upsilon in enumerate(upsilon_list):
+    out_map = radial.rhef(aia_map, upsilon=upsilon, method="scipy")
+    out_map_crop = out_map.submap(bottom_left, top_right=top_right)
+    out_map_crop.plot(axes=axes[i + 1])
+    axes[i + 1].set_title(f"Upsilon = {upsilon}")
+
+fig.tight_layout()
+
+#######################################################################################
+# Note that multiple filters can be used in a row to get a better output image.
+# Here, we will use both :func:`~.mgn` and :func:`~.wow`, then apply RHE filter after.
+
+mgn_map = enhance.mgn(aia_map)
+wow_map = enhance.wow(aia_map)
+
+rhef_map = radial.rhef(aia_map)
+
+rhef_mgn_map = radial.rhef(mgn_map)
+rhef_wow_map = radial.rhef(wow_map)
+
+fig, axes = plt.subplots(2, 3, figsize=(15, 10), sharex="all", sharey="all", subplot_kw={"projection": aia_map})
+axes = axes.flatten()
+
+rhef_map.plot(axes=axes[0])
+axes[0].set_title("RHEF(smap)")
+
+mgn_map.plot(axes=axes[1], norm=None)
+axes[1].set_title("MGN(smap)")
+
+wow_map.plot(axes=axes[2], norm=None)
+axes[2].set_title("WOW(smap)")
+
+toplot = (rhef_map.data + rhef_mgn_map.data) / 2
+combo_map = sunpy.map.Map(toplot, rhef_map.meta)
+combo_map.plot_settings["norm"] = None
+combo_map.plot(axes=axes[3])
+axes[3].set_title("AVG( RHEF(smap), RHEF(MGN(smap) )")
+
+rhef_mgn_map.plot(axes=axes[4])
+axes[4].set_title("RHEF( MGN(smap) )")
+
+rhef_wow_map.plot(axes=axes[5])
+axes[5].set_title("RHEF( WOW(smap) )")
+
+fig.tight_layout()
+print("Done!")
+
+plt.show()
