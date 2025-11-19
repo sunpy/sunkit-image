1ba17c42c187273d5227853173db4bc7cdd27462
Nabil Freij <nabil.freij@gmail.com>
2025-11-19 09:45:20 -0800

    Update sunkit_image/coalignment/tests/test_match_template.py
    Co-authored-by: Will Barnes <will.t.barnes@gmail.com>

--- sunkit_image/coalignment/tests/test_match_template.py
+++ sunkit_image/coalignment/tests/test_match_template.py
@@ -15,34 +15,6 @@ from sunkit_image.coalignment.match_template import (
 
 
 
-def test_get_correlation_shifts():
-    # Input array is 3 by 3, the most common case
-    test_array = np.zeros((3, 3))
-    test_array[1, 1] = 1
-    test_array[2, 1] = 0.6
-    test_array[1, 2] = 0.2
-    y_test, x_test = _get_correlation_shifts(test_array)
-    assert_allclose(y_test, 0.214285714286, rtol=1e-2, atol=0)
-    assert_allclose(x_test, 0.0555555555556, rtol=1e-2, atol=0)
-
-    # Input array is smaller in one direction than the other.
-    test_array = np.zeros((2, 2))
-    test_array[0, 0] = 0.1
-    test_array[0, 1] = 0.2
-    test_array[1, 0] = 0.4
-    test_array[1, 1] = 0.3
-    y_test, x_test = _get_correlation_shifts(test_array)
-    assert_allclose(y_test, 1.0, rtol=1e-2, atol=0)
-    assert_allclose(x_test, 0.0, rtol=1e-2, atol=0)
-
-    # Input array is too big in either direction
-    test_array = np.zeros((4, 3))
-    with pytest.raises(ValueError, match=r"Input array dimension should not be greater than 3 in any dimension."):
-        _get_correlation_shifts(test_array)
-
-    test_array = np.zeros((3, 4))
-    with pytest.raises(ValueError, match=r"Input array dimension should not be greater than 3 in any dimension."):
-        _get_correlation_shifts(test_array)
 
 
 def test_find_best_match_location(aia171_test_map, aia171_test_template, aia171_test_shift):
